c8bd16a042009f556c8b90b8aad1ed11abaab0ae
Filip Wojciechowski <fwojciec@gmail.com>
2021-01-24 21:24:59 +0100

    add integration tests to ci

--- automation/deploy-ci.sh
+++ automation/deploy-ci.sh
@@ -2,12 +2,17 @@
 
 set -euo pipefail
 
+STACK_NAME=serverless-template-python-ci
+
+CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text)
+if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
+  echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
+  exit 1
+fi
+
 sam deploy \
-        --stack-name serverless-template-python-ci \
+        --stack-name $STACK_NAME \
         --template-file ci.yaml \
         --capabilities CAPABILITY_IAM \
+        --parameter-overrides CloudformationArtifactsBucket=$CLOUDFORMATION_ARTIFACTS_BUCKET \
         --no-fail-on-empty-changeset
-
-echo
-echo Stack deployed as serverless-template-python-ci
-echo
