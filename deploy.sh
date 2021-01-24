8ca0e485934920fa720bad77bab5c2ce91d9e4b9
Filip Wojciechowski <fwojciec@gmail.com>
2021-01-24 21:24:35 +0100

    custom deployment script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -euo pipefail
+
+if [ "$#" -gt 0 ]; then
+  STACK_NAME=$1
+else
+  STACK_NAME="serverless-template-python-${USER}"
+fi
+
+CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text)
+if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
+  echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
+  exit 1
+fi
+
+echo "Deploying stack $STACK_NAME"
+echo "Deploying using Cloudformation artifacts bucket $CLOUDFORMATION_ARTIFACTS_BUCKET"
+echo
+
+sam deploy \
+        --stack-name $STACK_NAME \
+        --s3-bucket $CLOUDFORMATION_ARTIFACTS_BUCKET \
+        --template-file template.yaml \
+        --capabilities CAPABILITY_IAM \
+        --no-fail-on-empty-changeset
